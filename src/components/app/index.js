eb27ece25ec56f75807dba13577512725e73a727
Meghna <meggie@Rajs-MacBook-Pro.local>
2018-02-12 12:37:16 +0100

    added react-router to navigate to another component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,58 +1,60 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import _ from 'lodash';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gridsize: '3',
-      user_symbol: '',
-      computer_symbol: '',
-      error: '',
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    const { gridElement, playerSymbol, compSymbol } = this.refs;
-    const validationResult = playerSymbol.value === compSymbol.value;
-    console.log('result', gridElement.value, playerSymbol.value, compSymbol.value, validationResult);
-
-    if (validationResult) {
-      const error = "The symbols cannot be same.";
-      this.setState({ error })
-      return;
+    constructor(props) {
+        super(props);
+        this.state = {
+            gridsize: '3',
+            user_symbol: '',
+            computer_symbol: '',
+            error: '',
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-  }
 
-  render() {
-    return (
-      <div className="tic-tac-toe">
-        <h1>Tic Tac Toe</h1>
-        <div className="boardsize">
-          <form onSubmit={this.handleSubmit}>
-            <label className="button button1">
-              <select ref="gridElement" className="styled-select green rounded" defaultValue={this.state.gridsize}>
-                <option value="3">3 X 3</option>
-                <option value="4">4 X 4</option>
-                <option value="5">5 X 5</option>
-                <option value="6">6 X 6</option>
-                <option value="7">7 X 7</option>
-                <option value="8">8 X 8</option>
-                <option value="9">9 X 9</option>
-                <option value="10">10 X 10</option>
-              </select>
-            </label>
-            <input ref="playerSymbol" type="text" className="symbol" placeholder="Your Symbol" />
-            <input ref="compSymbol" type="text" className="symbol" placeholder="Opponent symbol" />
-            {this.state.error}
-            <input type="submit" value="Submit" onSubmit={this.handleSubmit} />
-          </form>
-        </div>
-      </div>		      
-    );		     
-  }
-}
-	 
-export default App;
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const { gridElement, playerSymbol, compSymbol } = this.refs;
+        const validationResult = playerSymbol.value === compSymbol.value;
+        console.log('result', gridElement.value, playerSymbol.value, compSymbol.value, validationResult);
+
+        if (validationResult) {
+            const error = "The symbols cannot be same.";
+
+            this.setState({ error })
+            return;
+        }
+        this.props.history.push('/game');
+    }
+
+    render() {
+        return (
+            <div className="tic-tac-toe">
+                <h1>Tic Tac Toe</h1>
+                <div className="boardsize">
+                    <form onSubmit={this.handleSubmit}>
+                        <label className="button button1">
+                            <select ref="gridElement" className="styled-select green rounded" defaultValue="3">
+                                {this.getOptions(3, 10)}
+                            </select>
+                        </label>
+                        <input ref="playerSymbol" type="text" className="symbol" placeholder="Your Symbol" />
+                        <input ref="compSymbol" type="text" className="symbol" placeholder="Opponent symbol" />
+                        {this.state.error}
+                        <input type="submit" value="Submit" onSubmit={this.handleSubmit} />
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+    getOptions(start, end) {
+        const items = _.range(start, end + 1);
+        return _.map(items, (i) => <option value={i} key={i}> {i} x {i} </option>);
+    }
+}		  
+
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/app';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/game" render={() => <h1>Hello Game</h1>} />
+        </Switch>
+    </Router>, document.getElementById('root')
+);
