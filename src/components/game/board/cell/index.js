07aa165ad11f421d86ba5549d72e82570fd20d85
Meghna <meggie@Rajs-MacBook-Pro.local>
2018-02-12 16:05:56 +0100

    modified board and cell

--- src/components/game/board/cell/index.js
+++ src/components/game/board/cell/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Cell = (props) => {
-    const pos = props.row + "-" +props.column
-    return (<div key={pos} className="cell">{pos}</div>)
+    const pos = props.row + "-" + props.column
+    return (
+        <button key={pos} className="cell">
+            {pos}
+        </button>
+    );
 }
 
 export default Cell;
--- src/components/game/board/index.js
+++ src/components/game/board/index.js
@@ -5,32 +5,30 @@ import Cell from './cell';
 class Board extends Component {
     render(props) {
         console.log("grid_size", this.props.grid_size);
-        
+
         const rows = _.range(0, this.props.grid_size);
         const columns = _.range(0, this.props.grid_size);
         return (
-            <div className="board">
-                <table><tbody>
-                    {// Iterate for each row
-                      rows.map((value, row_index) => {
+            <table className="board"><tbody>
+                {// Iterate for each row
+                    rows.map((value, row_index) => {
                         return (
                             <tr key={row_index}>
                                 {// Iterate for each column
-                                  columns.map((value, column_index) => {
-                                    // Add cell for every position.
-                                    return (
-                                        <td key={column_index}>
-                                            <Cell row={row_index} column={column_index} />
-                                        </td>
-                                    )
-                                  })
+                                    columns.map((value, column_index) => {
+                                        // Add cell for every position.
+                                        return (
+                                            <td key={column_index}>
+                                                <Cell row={row_index} column={column_index} />
+                                            </td>
+                                        )
+                                    })
                                 }
                             </tr>
                         )
-                      })
-                    }
-                </tbody></table>
-            </div>
+                    })
+                }
+            </tbody></table>
         )
     }
 }
